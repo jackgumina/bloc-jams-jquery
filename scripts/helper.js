4c8f45bd99560c6fbccc2e3da5038913fd74f194
Jack <jackgumina@gmail.com>
2019-12-22 15:34:09 -0600

    assignment-12.18 first commit

---
+++ scripts/helper.js
@@ -0,0 +1,9 @@
+class Helper {
+  playPauseAndUpdate(song) {
+    const duration = player.getDuration(song);
+    $('#time-control .total-time').text( duration );
+    player.playPause(song);
+  }
+}
+
+const helper = new Helper ();
